16c9c79c6a2efaeced9f9171e2e1017273b0f8c4
gcmsp852-commits <gcmsp852@gmail.com>
2026-07-14 22:59:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,46 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = "qr-matrix-reader-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./jsQR.js",
+  "./manifest.json",
+  "./icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== self.location.origin) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        if (response && response.ok) {
+          const copy = response.clone();
+          event.waitUntil(
+            caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy))
+          );
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
